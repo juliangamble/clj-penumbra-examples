6b735a958f684ae435c56ae166ca1d95658fa7e1
Julian Gamble <mail@juliangamble.com>
2012-02-18 21:10:19 +1100

    Added asteroids

--- src/example/gpgpu/importer.clj
+++ src/example/gpgpu/importer.clj
@@ -3,6 +3,7 @@
             [example.gpgpu.convolution :as c]
             [example.gpgpu.brians-brain :as brian]
             [example.opengl.marble :as tea]
+            [example.game.asteroids :as asteroids]
             [example.gpgpu.fluid :as fluid])
   (:gen-class
     :main main))
